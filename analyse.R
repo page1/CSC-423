8d215cb18e8e561e6ccc97784295c33ee1085946
Scott Page <scott@mytastebud.com>
2015-06-07 22:07:32 -0500

    CV

--- analyse.R
+++ analyse.R
@@ -1,4 +1,5 @@
 library(ggplot2)
+library(cvTools)
 
 data_summary <- function(data){
   player_count <- data %>%
@@ -96,10 +97,49 @@ model_silver_slug <- function(data){
     filter(complete.cases(.))
 
   model <- glm(win_silver_slug ~ .-playerID -yearID, data = batting_data, family = binomial())
-  #model_backwards_selection <- step(model, direction = "backward", trace = F)
+  model_backwards_selection <- step(model, direction = "backward", trace = F)
   model_backwards_linear <- lm(win_silver_slug ~ b_G + b_G_batting + b_R + b_H + b_HR + b_RBI + b_CS + b_BB + b_SO + b_HBP + b_home_runs_per_H + b_balls_per_AB + b_HBP_per_AB + b_games_batted_per_all_games, data = batting_data)
   model_backwards_semifinal <- glm(win_silver_slug ~b_H + b_HR + b_CS + b_BB + b_SO + b_HBP + b_home_runs_per_H + b_balls_per_AB + b_HBP_per_AB + b_games_batted_per_all_games, data = batting_data,  family = binomial())
   model_backwards_final <- glm(win_silver_slug ~b_H + b_HR + b_BB + b_SO + b_home_runs_per_H + b_balls_per_AB, data = batting_data,  family = binomial())
+  model_backwards_final_lm <- lm(win_silver_slug ~b_H + b_HR + b_BB + b_SO + b_home_runs_per_H + b_balls_per_AB, data = batting_data,  family = binomial())
+  
+  k <- 5
+  r <- 5
+  folds <- cvFolds(nrow(batting_data), K = k, R = r)
+  cross_validated_results <- lapply(1:r, function(R){
+    lapply(1:k, function(K){
+      print(paste("R=", R, " K=", K, sep = ""))
+      train <- batting_data[folds$subsets[folds$which != K, R],]
+      test <- batting_data[folds$subsets[folds$which == K, R],]
+      model <- glm(win_silver_slug ~b_H + b_HR + b_BB + b_SO + b_home_runs_per_H + b_balls_per_AB, data = train,  family = binomial())
+      
+      cut_offs <- seq(0, 1, by = 0.01)
+      results_at_cut_off <- find_cut_off_logistic(model, cut_offs, train, which(colnames(train) == "win_silver_slug")[1])
+      
+      best_cut_off <- results_at_cut_off %>%
+        filter(f_measure == max(f_measure, na.rm = T)) %>%
+        filter(precision == max(precision, na.rm = T))
+      
+      cut_off <- best_cut_off$cut_off
+      
+      predicted <- predict(model, newdata = test, type = "response")
+      
+      results <- data.frame(
+        true_positive = sum((predicted >= cut_off) & test$win_silver_slug),
+        true_negative = sum(!(predicted >= cut_off) & !test$win_silver_slug),
+        false_positive = sum((predicted >= cut_off) & !test$win_silver_slug),
+        false_negative = sum(!(predicted >= cut_off) & test$win_silver_slug))
+      
+      return(results)
+    }) %>% do.call("rbind", .)
+  }) %>% do.call("rbind", .) %>%
+    mutate(recall = true_positive / (true_positive + false_negative),
+           accuracy = (true_positive + true_negative) / (true_positive + true_negative + false_positive + false_negative),
+           precision = true_positive / (true_positive + false_positive),
+           specificity = true_negative / (true_negative + false_positive),
+           f_measure = (2 * precision * recall) / (recall + precision),
+           prior = (true_positive + false_negative) / (true_positive + true_negative + false_positive + false_negative),
+           lift = precision / prior)
   
   cut_offs <- seq(0, 1, by = 0.01)
   results_at_cut_off <- find_cut_off_logistic(model_backwards_semifinal, cut_offs, batting_data, which(colnames(batting_data) == "win_silver_slug")[1])
@@ -124,6 +164,8 @@ model_silver_slug <- function(data){
        model_backwards_linear,
        model_backwards_final,
        model_backwards_semifinal,
+       model_backwards_final_lm,
+       cross_validated_results,
        best_cut_off,
        plot_of_cut_offs,
        file = "silver_slug_model.Rdata")
